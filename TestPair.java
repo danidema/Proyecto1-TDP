399e3304cdbff6172ee6c045e92b3b33a6d75e6a
danidema <daniel.de.mattia.7@gmail.com>
2020-09-13 20:37:05 -0300

    inciso j cambio

--- TestPair.java
+++ TestPair.java
@@ -2,7 +2,7 @@
 public class TestPair {
 
 	public static void main(String[] args) {
-		Pair<Integer, String> p = new Pair<Integer, String>(23,"juan");
+		Pair<Integer, String> p = new Pair<Integer, String>(23,"marcos");
 		Pair<Float, Long> q = new Pair<Float, Long>(23.2f, 21345l);
 		System.out.println("p: "+ p.toString());
 		System.out.println("q: "+ q.toString());
